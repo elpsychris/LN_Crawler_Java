6b68880ef4d69ecd4c21fa769aa71d275018df0b
Tung Nguyên <rohirrim3105@gmail.com>
2018-06-15 09:03:14 +0700

    add util funct for getting IStream from url

--- src/main/java/crawler/utils/ComUtils.java
+++ src/main/java/crawler/utils/ComUtils.java
@@ -1,10 +1,57 @@
 package crawler.utils;
 
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
 
 public class ComUtils {
+    private final static String USER_AGENT_HEADER = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11";
+    private final static int RETRY_INTERVAL = 10000;
+    private final static int TIMEOUT = 6000;
+
+
     public static Date getCurrentDate() {
         return Calendar.getInstance().getTime();
     }
+
+    public static InputStream getHttp(String uri) throws IOException, InterruptedException {
+        URL url = new URL(uri);
+        InputStream httpResult = null;
+        URLConnection connection = openConnection(url);
+
+        boolean cont = true;
+        while (cont) {
+            try {
+                httpResult = connection.getInputStream();
+                cont = false;
+                Thread.sleep(1000);
+            } catch (IOException e) {
+                System.out.println("Failed to connect to " + url);
+                System.out.println(e.getMessage());
+
+                openConnection(url);
+
+                Thread.sleep(RETRY_INTERVAL);
+                cont = true;
+
+            }
+        }
+
+        return httpResult;
+    }
+
+    private static URLConnection openConnection(URL url) throws IOException {
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent", USER_AGENT_HEADER);
+        connection.setReadTimeout(1000);
+
+        return connection;
+    }
+
+
 }
--- src/main/java/crawler/utils/MyURIResolver.java
+++ src/main/java/crawler/utils/MyURIResolver.java
@@ -1,4 +1,58 @@
 package crawler.utils;
 
-public class MyURIResolver {
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+
+public class MyURIResolver implements URIResolver {
+    private final static String XML_DECLARATION = "?xml version='1.0' encoding='UTF-8' standalone='no' ?>";
+
+    @Override
+    public Source resolve(String href, String base) throws TransformerException {
+        if (href != null && href.indexOf("https://ln.hako.re/danh-sach?page=1") == 0) {
+            try {
+                InputStream httpResult = ComUtils.getHttp(href);
+                StreamSource streamSource = preProcessInputStream(httpResult);
+                return streamSource;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    private StreamSource preProcessInputStream(InputStream httpResult) throws IOException {
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStreamReader inputStreamReader = new InputStreamReader(httpResult, "UTF-8");
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        stringBuffer.append(XML_DECLARATION + "\n");
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.contains("<html") && line.contains("xmlns=\"http://www.w3.org/1999/xhtml\"")) {
+                line = line.replace("<html", "<html xmlns=\"http://www.w3.org/1999/xhtml\"");
+            }
+            if (line.contains("src") || line.contains("href")) {
+                line = line.replace("&", "&amp;");
+            }
+
+            // (R) char and ... and non-breaking space char replacement
+            line = line
+                    .replace("&reg;", "&#174;")
+                    .replace("&hellip;", "")
+                    .replace("&nbsp;", "");
+
+            stringBuffer.append(line + "\n");
+        }
+
+        InputStream htmlResult = new ByteArrayInputStream(stringBuffer.toString().getBytes());
+        return new StreamSource(htmlResult);
+    }
+
 }
