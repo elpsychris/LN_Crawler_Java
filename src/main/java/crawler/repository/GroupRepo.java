839f08d186c6036df4f799d6217b721c6be0e599
Tung Nguyên <rohirrim3105@gmail.com>
2018-06-23 09:51:58 +0700

    add findExist to GroupRepo

--- src/main/java/crawler/repository/GroupRepo.java
+++ src/main/java/crawler/repository/GroupRepo.java
@@ -1,4 +1,20 @@
 package crawler.repository;
 
-public class GroupRepo {
+import crawler.model.GroupEntity;
+
+public class GroupRepo extends AbstractRepo<GroupEntity> {
+    public GroupRepo() {
+        this.idKey = "groupId";
+    }
+
+    public GroupEntity findExist(GroupEntity obj) {
+        try {
+            return this.findExist(GroupEntity.class, obj.getGroupId());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
